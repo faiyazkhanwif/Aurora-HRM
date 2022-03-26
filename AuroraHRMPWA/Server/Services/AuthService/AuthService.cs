dbc11f683ccc955b0394cd53b41f1da2e512b5d7
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-03-26 18:00:39 +0800

    Removed unnecessary package, added IAuthService

--- AuroraHRMPWA/Server/Services/AuthService/AuthService.cs
+++ AuroraHRMPWA/Server/Services/AuthService/AuthService.cs
@@ -1,8 +1,20 @@
-﻿namespace AuroraHRMPWA.Server.Services.AuthService
+﻿
+namespace AuroraHRMPWA.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
+        private readonly DataContext _context;
+
+        public AuthService(DataContext context)
+        {
+            _context = context;
+        }
         public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string email)
         {
             throw new NotImplementedException();
         }
--- AuroraHRMPWA/Server/Services/AuthService/IAuthService.cs
+++ AuroraHRMPWA/Server/Services/AuthService/IAuthService.cs
@@ -3,5 +3,6 @@
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(User user, string password);
+        Task<bool> UserExists(string email);
     }
 }
