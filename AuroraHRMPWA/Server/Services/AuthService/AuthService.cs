60f2714bb692722214f03fe236119702e38320e9
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-03-26 18:45:13 +0800

    Implemented AuthService

--- AuroraHRMPWA/Server/Services/AuthService/AuthService.cs
+++ AuroraHRMPWA/Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography;
+
 namespace AuroraHRMPWA.Server.Services.AuthService
 {
     public class AuthService : IAuthService
@@ -9,9 +11,28 @@ namespace AuroraHRMPWA.Server.Services.AuthService
         {
             _context = context;
         }
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            if(await UserExists(user.Email)){
+                return new ServiceResponse<int> 
+                { 
+                    Success = false,
+                    Message = "User already exists"
+                };
+            }
+
+            //out is used so that we can get values and set to the user.PasswordHash and user.PasswordSalt
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int>
+            {
+                Data = user.Id
+            };
         }
 
         public async Task<bool> UserExists(string email)
@@ -23,6 +44,20 @@ namespace AuroraHRMPWA.Server.Services.AuthService
                 return true;
             }
             return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            //hmacsha512 algorithm for encrypting password
+            //Giving our pass inside the algorithm,
+            //Algorithm instance creates a key which can be used for Password Salt
+            //The computeHash method will use the password and the key to create the pass hash
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
