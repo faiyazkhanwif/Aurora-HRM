7121816afaca1d485a371772f094cce2b175f058
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-03-28 12:52:47 +0800

    Added Login method for AuthServiceClient and installed Blazored.LocalStorage package

--- AuroraHRMPWA/Client/Program.cs
+++ AuroraHRMPWA/Client/Program.cs
@@ -2,6 +2,7 @@ global using AuroraHRMPWA.Client.Services.AuthService;
 global using AuroraHRMPWA.Shared;
 global using System.Net.Http.Json;
 using AuroraHRMPWA.Client;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAuthServiceClient, AuthServiceClient>();
 
--- AuroraHRMPWA/Client/Services/AuthService/IAuthServiceClient.cs
+++ AuroraHRMPWA/Client/Services/AuthService/IAuthServiceClient.cs
@@ -4,5 +4,6 @@ namespace AuroraHRMPWA.Client.Services.AuthService
     public interface IAuthServiceClient
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
+        Task<ServiceResponse<string>> Login(UserLogin request);
     }
 }
